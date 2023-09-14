3f33f8b8cbf379b6c638ea8945fe7b790fb8f64c
Psychopixel <124247646+Psychopixel@users.noreply.github.com>
2023-09-14 17:56:19 +0200

    move calculate move on hexgridlayout

--- hexGridLayout.py
+++ hexGridLayout.py
@@ -17,6 +17,34 @@ class HexGridLayout(FloatLayout):
         self.yRange = yRange
         self.versus = versus
 
+        self.horizontalGridOddRowRange = [
+            # even rows
+            [[+1, 0], [+1, +1], [0, +1], [-1, 0], [0, -1], [+1, -1]],
+            # odd rows
+            [[+1, 0], [0, +1], [-1, +1], [-1, 0], [-1, -1], [0, -1]],
+        ]
+
+        self.horizontalGridEvenRowRange = [
+            # even rows
+            [[+1, 0], [0, +1], [-1, +1], [-1, 0], [-1, -1], [0, -1]],
+            # odd rows
+            [[+1, 0], [+1, +1], [0, +1], [-1, 0], [0, -1], [+1, -1]],
+        ]
+
+        self.verticalGridOddColRange = [
+            # even cols
+            [[+1, 0], [0, +1], [-1, 0], [-1, -1], [0, -1], [+1, -1]],
+            # odd cols
+            [[+1, +1], [0, +1], [-1, +1], [-1, 0], [0, -1], [+1, 0]],
+        ]
+
+        self.verticalGridEvenColRange = [
+            # even cols
+            [[+1, 0], [0, +1], [-1, 0], [-1, -1], [0, -1], [+1, -1]],
+            # odd cols
+            [[+1, +1], [0, +1], [-1, +1], [-1, 0], [0, -1], [+1, 0]],
+        ]
+
         # Set size and position hints
         self.size_hint = (None, None)
 
@@ -28,8 +56,9 @@ class HexGridLayout(FloatLayout):
             )
         else:
             self.size = (
-                self.hex_size * self.xRange + self.hex_size*0.866 * (self.xRange/2 + self.xRange%2),
-                self.hex_size * 0.866 * 2 * self.yRange + self.hex_size*0.866/2
+                self.hex_size * self.xRange
+                + self.hex_size * 0.866 * (self.xRange / 2 + self.xRange % 2),
+                self.hex_size * 0.866 * 2 * self.yRange + self.hex_size * 0.866 / 2,
             )
 
         self.pos_hint = {"center_x": 0.5, "center_y": 0.5}
@@ -105,6 +134,105 @@ class HexGridLayout(FloatLayout):
             self.ids[hex_name].center_x + self.arrow.arrow_button.width / 2,
             self.ids[hex_name].center_y + self.arrow.arrow_button.height / 2,
         )
+
+    def getAllContiguos(self, yRange, xRange, arrow_y, arrow_x, gridVersus):
+        neibourghs = []
+        parity_row = arrow_y & 1
+        parity_col = arrow_x & 1
+        even = 0
+        odd = 1
+        if gridVersus == HexGridType.HORIZONTAL:
+            # odd_r or even_r
+            if yRange % 2 == 0:
+                # even_r
+                if parity_row == 0:
+                    # even row
+                    neibourghs = self.horizontalGridEvenRowRange[even].copy()
+                else:
+                    # odd row
+                    neibourghs = self.horizontalGridEvenRowRange[odd].copy()
+            else:
+                # odd_r
+                if parity_row == 0:
+                    # even row
+                    neibourghs = self.horizontalGridOddRowRange[even].copy()
+                else:
+                    # odd row
+                    neibourghs = self.horizontalGridOddRowRange[odd].copy()
+        else:
+            # odd_q or even_q
+            if xRange % 2 == 0:
+                # even_q
+                if parity_col == 0:
+                    # even col
+                    neibourghs = self.verticalGridEvenColRange[even].copy()
+                else:
+                    # odd col
+                    neibourghs = self.verticalGridEvenColRange[odd].copy()
+            else:
+                # odd_q
+                if parity_col == 0:
+                    # even col
+                    neibourghs = self.verticalGridOddColRange[even].copy()
+                else:
+                    # odd col
+                    neibourghs = self.verticalGridOddColRange[odd].copy()
+        return neibourghs
+
+    def getPossibleHex(self, contiguos, direction):
+        if direction == 0:
+            clickable = [contiguos[5], contiguos[0], contiguos[1]]
+        elif direction == 1:
+            clickable = [contiguos[0], contiguos[1], contiguos[2]]
+        elif direction == 2:
+            clickable = [contiguos[1], contiguos[2], contiguos[3]]
+        elif direction == 3:
+            clickable = [contiguos[2], contiguos[3], contiguos[4]]
+        elif direction == 4:
+            clickable = [contiguos[3], contiguos[4], contiguos[5]]
+        else:
+            clickable = [contiguos[4], contiguos[5], contiguos[0]]
+        return clickable
+
+    def getMove(self, possibleHex, hexClicked, arrow_x, arrow_y, direction):
+        cnt = 0
+        for possible in possibleHex:
+            newHex = [possible[0] + arrow_x, possible[1] + arrow_y]
+            if newHex == hexClicked:
+                if cnt == 0:
+                    newDirection = direction - 1
+                    if newDirection < 0:
+                        newDirection = 5
+                    return f"rotate_{newDirection}"
+                elif cnt == 1:
+                    return "move"
+                else:
+                    newDirection = direction + 1
+                    if newDirection > 5:
+                        newDirection = 0
+                    return f"rotate_{newDirection}"
+            cnt += 1
+        return None
+
+    def calculateMove(self, instance):
+        arrow_x, arrow_y, arrow_direction = self.getArrow()
+        move = self.getMove(
+            self.getPossibleHex(
+                self.getAllContiguos(
+                    yRange=self.yRange,
+                    xRange=self.xRange,
+                    arrow_x=arrow_x,
+                    arrow_y=arrow_y,
+                    gridVersus=self.versus,
+                ),
+                direction=arrow_direction,
+            ),
+            [instance.xCoord, instance.yCoord],
+            arrow_x=arrow_x,
+            arrow_y=arrow_y,
+            direction=arrow_direction,
+        )
+        return move
 
     def do_layout(self, *args):
         pass
